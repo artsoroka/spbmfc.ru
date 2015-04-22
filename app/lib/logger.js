d1385780c3668fe0527a5b6a4f3cea7db37b8c36
artsoroka <githubrepository@yandex.ru>
2015-04-22 16:53:40 +0000

    added winston log

---
+++ app/lib/logger.js
@@ -0,0 +1,14 @@
+var winston = require("winston"); 
+var config  = require("../config"); 
+
+module.exports = new (winston.Logger)({
+    transports: [
+      
+      new (winston.transports.Console)(),
+      
+      new (winston.transports.File)({ 
+          filename: config.logger.path 
+      })
+    
+    ]
+  });
